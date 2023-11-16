adda218fcf319ec1cc52917e515e99954be1acff
Batuhan ÖKMEN <45328109+flavves@users.noreply.github.com>
2023-11-16 14:16:12 +0300

    Add files via upload

--- webhookapi.py
+++ webhookapi.py
@@ -1,12 +1,8 @@
-from flask import Flask, request, jsonify
-
+from flask import Flask, request, Response
 app = Flask(__name__)
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.get_json()
-    print('Webhook alındı:', data)
-    return jsonify({'message': 'Webhook başarıyla alındı'})
-
-if __name__ == '__main__':
-    app.run(port=3000)
+@app.route('/my_webhook', methods=['POST'])
+def return_response():
+    print(request.json);
+    ## Do something with the request.json data.
+    return Response(status=200)
+if __name__ == "__main__": app.run()
